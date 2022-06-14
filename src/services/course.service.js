e1c2137671695871cb0668b10b53879750b2e7af
NENG0910 <85937816+NENG0910@users.noreply.github.com>
2022-06-14 17:15:48 +0800

    url update

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://vast-falls-97220.herokuapp.com//api/courses";
 class CourseService {
   //讓axios攜帶jwt去server
   postCourse(title, description, price) {
